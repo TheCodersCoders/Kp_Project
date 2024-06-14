c347cca81880cb9a89266dcfa1859f89b683d0ae
George0717 <raden7george@gmail.com>
2024-06-15 02:16:55 +0700

    create mutasi progress UI

--- resources/views/mutasi/create.blade.php
+++ resources/views/mutasi/create.blade.php
@@ -29,6 +29,48 @@
     </div>
   </div>
  {{-- Akhir Tujuan --}}
+ {{-- Data Mutasi --}}
+ <div class="row">
+  <h3>Tambah Data</h3>
+  <div class="col-md-6">
+    <div class="input-group input-group-outline my-3">
+      <input type="text" class="form-control" placeholder="Nama Barang">
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="input-group input-group-outline my-3">
+      <input type="text" class="form-control" placeholder="Model">
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-6">
+    <div class="input-group input-group-outline my-3">
+      <input type="text" class="form-control" placeholder="Model`">
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="input-group input-group-outline my-3">
+      <input type="text" class="form-control" placeholder="Nomor Inventaris">
+    </div>
+  </div>
+  <div class="col-md-12">
+    <select class="form-select" aria-label="Default select example" aria-placeholder="Kategori">
+      <option value="1" > Baru</option>
+      <option value="2"> Mutasi</option>
+      <option value="3"> Rusak</option>
+    </select>
+  </div>
+  <div class="col-md-12">
+    <div class="input-group input-group-outline my-3">
+      <input type="text" class="form-control" placeholder="Keterangan">
+    </div>
+  </div>
+</div>
+ {{-- Akhir Data Mutasi --}}
+ <div class="text-center col-md-3 justify-align-center">
+  <button type="submit" class="btn bg-gradient-primary btn-lg w-100 mt-1 mb-0">Submit</button>
+</div>
 </form>
 
 @endsection
